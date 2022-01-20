37b690d33c9ef595a014c35cd791a4aabe79db66
michaelst15 <misuto991@gmail.com>
2022-01-20 09:50:13 +0700

    Task Promise

--- promise/TaskPromise.js
+++ promise/TaskPromise.js
@@ -1,8 +1,24 @@
-const url = "https://newsapi.org/v2/everything?q=apple&from=2022-01-10&to=2022-01-10&sortBy=popularity&apiKey=1e4d7358c26843d98c618391469576e2";
-axios.get(url)
-  .then(function (response) {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
+const box = document.getElementById('data');
+const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1e4d7358c26843d98c618391469576e2";
+axios.get(url)                                         
+  .then(response => {
+    box.innerHTML = render(response.data);
+  })                                          
+  .catch(error => {
+    box.innerHTML = error;
   });
+                                        
+
+
+function render(response) {
+let box = '';
+response.articles.forEach(data => {
+  box +=  `<div class="container bg-dark mt-5">
+              <div class="img"><img src="${data.urlToImage}" style="width:100%";/></div><br>
+              <div class="title text-white"><u><i>${data.title}</i></u></div><br>
+              <div class="publishedAt text-white">${data.publishedAt}</div><br>
+              <div class="description text-white">${data.description}</div><br>
+           </div>` ;
+});
+  return box;
+};
