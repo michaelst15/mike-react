3bc0addd8a8ea9be0724ea9453548d7146fa5ca1
michaelst15 <misuto991@gmail.com>
2022-01-20 13:55:54 +0700

    promise

--- promise/TaskPromise.js
+++ promise/TaskPromise.js
@@ -1,15 +1,15 @@
 const box = document.getElementById('data');
-const url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1e4d7358c26843d98c618391469576e2";
+const search = document.getElementById('Search');
+const url = "https://newsapi.org/v2/everything?q=tesla&from=2021-12-20&sortBy=publishedAt&apiKey=1e4d7358c26843d98c618391469576e2";
 axios.get(url)                                         
   .then(response => {
     box.innerHTML = render(response.data);
   })                                          
   .catch(error => {
-    box.innerHTML = error;
+    box.innerHTML = message(error.message);
   });
-                                        
 
-
+         
 function render(response) {
 let box = '';
 response.articles.forEach(data => {
@@ -22,3 +22,13 @@ response.articles.forEach(data => {
 });
   return box;
 };
+
+
+// const url = "https://newsapi.org/v2/everything?q=apple&from=2022-01-10&to=2022-01-10&sortBy=popularity&apiKey=1e4d7358c26843d98c618391469576e2";
+// axios.get(url)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
